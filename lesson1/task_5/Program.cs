5fac7727f6e8a8113dcc179342de3193dbdf72dc
Katya <katya6040@gmail.com>
2023-01-16 21:22:16 +0300

    Task 5

---
+++ lesson1/task_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Екатерина Юрьевна")
+{
+    Console.Write("Екатерина Юрьевна, выздоравливайте!");
+}
+else 
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
